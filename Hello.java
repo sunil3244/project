56f9b7c0837b51c9cdd51d0b33ce31ccb3178918
sunil3244 <91534862+sunil3244@users.noreply.github.com>
2021-09-28 22:52:34 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,15 @@
+
+/*Q 1 Write a Java program to print 'Hello' on screen
+   and then print your name on a separate line.*/
+ 
+package Assignment_01;
+
+public class Hello {
+
+	public static void main(String[] args)
+	{
+		System.out.println("hello");
+		System.out.println("sunil");
+       }
+
+}
